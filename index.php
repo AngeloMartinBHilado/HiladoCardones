b54ee303caaeb670d4c8029278601e9150cc9260
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-11 09:03:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="urf=8"?>
+<rss version="2.0">
+<channel>
+<title>Dota 2</title>
+
+<link><link rel="alternate" type="application/rss+xml" title="Dota 2 &raquo; Feed" href="http://blog.dota2.com/feed/" />
