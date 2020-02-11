1de8977f4c3507cf4ef25eff9e8edd4de31171c4
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-11 09:21:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?xml version="1.0" encoding="urf=8"?>
+<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
-<channel>
-<title>Dota 2</title>
 
-<link><link rel="alternate" type="application/rss+xml" title="Dota 2 &raquo; Feed" href="http://blog.dota2.com/feed/" /></link>
-  </channel>
+<channel>
+  <title>W3Schools Home Page</title>
+  <link>https://www.w3schools.com</link>
+  <description>Free web building tutorials</description>
+  <item>
+    <title>RSS Tutorial</title>
+    <link>https://www.w3schools.com/xml/xml_rss.asp</link>
+    <description>New RSS tutorial on W3Schools</description>
+  </item>
+  <item>
+    <title>XML Tutorial</title>
+    <link>https://www.w3schools.com/xml</link>
+    <description>New XML tutorial on W3Schools</description>
+  </item>
+</channel>
+
+</rss>
