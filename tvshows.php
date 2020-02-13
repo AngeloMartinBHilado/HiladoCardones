565a60371ca61706a4aa0df5e2dedbbf8595d2bc
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-13 10:46:16 +0800

    Update tvshows.php

--- tvshows.php
+++ tvshows.php
@@ -13,7 +13,6 @@ foreach( $content as $data )
    $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
    $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-   $category = $data->getElementsByTagName("category")->item(0)->nodeValue;
    $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
 
    ?>
@@ -29,7 +28,7 @@ foreach( $content as $data )
                                 <ul class="article-content">
                                   <li> <b> Link: </b> <br><a href="<?php echo $link;?>" target="blank"> <?php echo $link;?>  </a> </li>
                                   <li> <b> Published Date: </b><br> <?php echo $pubDate ?></li>
-                                  <li> <b> Category: </b> <br><?php echo $category; ?></li>
+                                 
 
                                 </li>
                                 </ul>
